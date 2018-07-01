47cd2b04c73798e0999357e86ba8fffb7c137d9e
Eric Walz <walzer85@gmail.com>
2018-06-30 21:45:55 -0500

    linked the sections.  Still need to decide whether I want to make separate pages, or even separate into componenets

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -12,22 +12,23 @@ class App extends Component {
             <header className="App-header">
             <img className="Header-image" src='https://media.licdn.com/dms/image/C4D03AQGFAEKgswK2uw/profile-displayphoto-shrink_200_200/0?e=1535587200&v=beta&t=_86dSGvHxKpTApFQ4eIp1ogSzoflOT7iBqpk_4oMZB0'/>
               <div className="App-title">
-                <h1>Eric Walz</h1>
-                <h3>Web Developer | Educator | Musician</h3>
+                <div className='My-name'>Eric Walz</div>
+                <div className='My-description'>Web Developer | Educator | Musician</div>
               </div>
             </header>
             </Row>
             <Row className="Menu">
-              <Col xs className="Menu-link">Biography</Col>
-              <Col xs className="Menu-link">Education</Col>
-              <Col xs className="Menu-link">Job Experience</Col>
-              <Col xs className="Menu-link">Skills</Col>
+              <Col xs className="Menu-link"><a href="#Biography">Biography</a></Col>
+              <Col xs className="Menu-link"><a href="#Education">Education</a></Col>
+              <Col xs className="Menu-link"><a href="#Job Experience">Job Experience</a></Col>
+              <Col xs className="Menu-link"><a href="#Skills">Skills</a></Col>
+              {/* Still need to decide how to handle these two portions.  Separate pages? */}
               <Col xs className="Menu-link">Contact</Col>
               <Col xs className="Menu-link">Media</Col>
 
             </Row>
         </Navbar>
-        <Jumbotron className="Biography">
+        <Jumbotron className="Biography" id="Biography">
         <h1>Biography</h1>
         <p>
         I am a seasoned problem solver and communicator that specializes in outreach and development of communication with clients. 
@@ -38,12 +39,12 @@ class App extends Component {
         I specialize in communicating between several departments and teams at a time and clarifying any information for relevance to each team.
         </p>
       </Jumbotron>
-      <Jumbotron className="Others">
+      <Jumbotron className="Others" id="Education">
         <h1>Education</h1>
         <p>Full Stack Web Design  |  Austin Coding Academy  |  Austin, TX, 2017 - 2018</p>
         <p>BA - Music Education  |  Magna Cum Laude  |  Virginia Commonwealth University  |  Richmond, Virginia, 2004 - 2008</p>
       </Jumbotron>
-      <Jumbotron className="Others">
+      <Jumbotron className="Others" id="Job Experience">
         <h1>Job Experience</h1>
         <h4>Client Care Specialist  |  Bazaarvoice  |  Austin, TX 2017 – 2018</h4>
           <p>Served as a central point of contact for IT-related help desk issues.  Worked with about 6000 clients.  Worked about 30-50 cases a day.  Helped manage workloads for colleagues in a fast-paced work environment.  Kept up to date documentation regarding changing processes in software support.  Received regular customer satisfaction results of 9 out of 10.  Responded to calls, tickets and emails to troubleshoot reported issues.  Developed product knowledge across six major products.  Trained customers on four of Bazaarvoice’s major products.  Effectively communicated problem status updates and resolution with internal and external clients.  Provided application and system configuration support and collected relevant details to facilitate issue resolution.  Supported internal company product knowledge, ensuring that other departments were provided with quick answers to their questions.  Clarified client communication to other teams to ensure the most accurate information was being shared.</p>
@@ -58,26 +59,32 @@ class App extends Component {
         <h4>Instrumental Music Director  |  Allen Academy  |  Bryan, Texas, 2008 - 2010</h4>
           <p>Led Upper School Band to Superior ratings in solo and ensemble categories in the Texas Association for Private and Parochial Schools Music Competition two years in a row, 3rd place overall in 2010.  Taught students of many socio-economic and cultural backgrounds, including many different learning styles.  Taught and developed new curriculum, scope, and sequence for Intro to World Music course for 9th - 12th grades.  Organized volunteer opportunities for the students with a local cultural festival.  Helped coordinate Peer Mentoring program between the Lower School and the Upper School.  Acted as Faculty Advisor to Middle School student council.  Maintained regular communication with students’ families regarding grades, behavior, etc.  Managed a growing library of recordings and literature for grades 3 – 12.  Managed budget for music purchasing, instrument purchasing/repair, and funding for school trips.</p>
       </Jumbotron>
-      <div className="Skills">
+      <div className="Skills" id="Skills">
         <h1>Skills</h1>
         <Row>
           <Col xs>Javascript</Col>
           <Col xs>HTML</Col>
           <Col xs>CSS</Col>
-          <Col xs>Microsoft Office Suite</Col>
         </Row>
         <Row>
           <Col xs>WordPress</Col>
           <Col xs>Billings Pro Software</Col>
           <Col xs>Customer Service</Col>
-          <Col xs>Google Apps</Col>
-          <Col xs>Jira</Col>
         </Row>
         <Row>
           <Col xs>Agile Processes</Col>
           <Col xs>Salesforce</Col>
           <Col xs>UNIX Commands</Col>
+        </Row>
+        <Row>
+          <Col xs>Microsoft Office Suite</Col>
+          <Col xs>Google Apps</Col>
+          <Col xs>Jira</Col>
+        </Row>
+        <Row>
           <Col xs>Adobe Acrobat</Col>
+          <Col xs>  </Col>
+          <Col xs>  </Col>
         </Row>
       </div>
     </div>
